00b5e15cf90e5afcbd5aa155c0efecc9737db409
adamfris <adamfris@student.chalmers.se>
2022-12-01 16:55:37 +0100

    Design update using tailwind

---
+++ Admin/src/views/ManageView.vue
@@ -0,0 +1,18 @@
+<script>
+import Header from "../components/Header.vue"
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+    <div>
+        <header>
+            <Header />
+        </header>
+        
+    </div>
+</template>
---
+++ Admin/src/views/TrainView.vue
@@ -0,0 +1,27 @@
+<script>
+import Header from "../components/Header.vue"
+import File from "../components/FileUpload.vue"
+
+export default {
+  components: {
+    Header,
+    File
+  }
+}
+</script>
+
+
+<template>
+    <main>    
+        <header>
+            <Header />
+        </header>
+       
+        <File />
+
+    </main>
+</template>
+
+<style scoped>
+
+</style>
