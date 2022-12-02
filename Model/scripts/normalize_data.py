679c90739792f791b7d51213ba5fd807d041a4d7
Mislav Milicevic <mislav.milicevic22@gmail.com>
2022-12-02 23:55:37 +0100

    Normalize popularity column

--- Model/scripts/normalize_data.py
+++ Model/scripts/normalize_data.py
@@ -36,32 +36,32 @@ def drop_unused_columns(df):
 
 
 # Split genres into separate columns (e.g. 'genres'-> 'genre_western' and 'genre_comedy')
-def normalize_genres(df):
-    logger.info("Normalizing column: 'genres'")
+def transform_genres(df):
+    logger.info("Transforming column: 'genres'")
 
     genres = df.loc[:, 'genres']
-    genres_normalized = []
+    genres_transformed = []
 
     for genre in genres:
         data = json.loads(genre)
         names = list(map(lambda x: x['name'].replace(" ", "_").lower(), data))
-        genres_normalized.append(names)
+        genres_transformed.append(names)
 
 
-    logger.debug("Creating column: 'genres_normalized'")
+    logger.debug("Creating column: 'genres_transformed'")
 
-    df['genres_normalized'] = genres_normalized
+    df['genres_transformed'] = genres_transformed
 
-    logger.debug("One-hot encoding column: 'genres_normalized'")
+    logger.debug("One-hot encoding column: 'genres_transformed'")
 
     df1 = (
-        df['genres_normalized'].explode()
+        df['genres_transformed'].explode()
         .str.get_dummies().groupby(level=0).sum().add_prefix('genres_')
     )
 
-    logger.debug("Dropping columns: 'genres', 'genres_normalized'")
+    logger.debug("Dropping columns: 'genres', 'genres_transformed'")
 
-    return df.drop('genres', axis=1).drop('genres_normalized', axis=1).join(df1)
+    return df.drop('genres', axis=1).drop('genres_transformed', axis=1).join(df1)
 
 
 def map_country_to_region(country):
@@ -78,32 +78,42 @@ def map_country_to_region(country):
 
 
 # Split production countries into separate columns
-def normalize_production_countries(df):
-    logger.info("Normalizing column: 'production_countries'")
+def transform_production_countries(df):
+    logger.info("Transforming column: 'production_countries'")
 
     production_countries = df.loc[:, 'production_countries']
-    production_countries_normalized = []
+    production_countries_transformed = []
 
     for production_country in production_countries:
         data = json.loads(production_country)
         regions = set(map(map_country_to_region, data))
-        production_countries_normalized.append(regions)
+        production_countries_transformed.append(regions)
 
     
-    logger.debug("Creating column: 'production_countries_normalized'")
+    logger.debug("Creating column: 'production_countries_transformed'")
 
-    df['production_countries_normalized'] = production_countries_normalized
+    df['production_countries_transformed'] = production_countries_transformed
 
-    logger.debug("One-hot encoding column: 'production_countries_normalized'")
+    logger.debug("One-hot encoding column: 'production_countries_transformed'")
 
     df2 = (
-        df['production_countries_normalized'].explode()
+        df['production_countries_transformed'].explode()
         .str.get_dummies().groupby(level=0).sum().add_prefix('region_') # shortened it from production_country to region
     )
 
-    logger.debug("Dropping columns: 'production_countries', 'production_countries_normalized'")
+    logger.debug("Dropping columns: 'production_countries', 'production_countries_transformed'")
 
-    return df.drop('production_countries', axis=1).drop('production_countries_normalized', axis=1).join(df2)
+    return df.drop('production_countries', axis=1).drop('production_countries_transformed', axis=1).join(df2)
+
+
+def normalize_popularity(df):
+    logger.info("Normalizing column: 'popularity'")
+
+    normalized_popularity = (df['popularity'] - df['popularity'].min()) / (df['popularity'].max() - df['popularity'].min())
+
+    df['popularity'] = normalized_popularity
+
+    return df
 
 
 # Remove movies with 0 or null runtime
@@ -117,18 +127,19 @@ def remove_nulls(df):
 
 
 # Create dataframe from CSV file
-df = pd.read_csv('tmdb_5000_movies.csv', sep=',')
+df = pd.read_csv('data/tmdb_5000_movies.csv', sep=',')
 
 df = drop_unused_columns(df)
-df = normalize_genres(df)
-df = normalize_production_countries(df)
+df = transform_genres(df)
+df = transform_production_countries(df)
+df = normalize_popularity(df)
 df = remove_nulls(df)
 
 
 logger.debug("Saving transformations to 'normalized.csv'")
 
 # Create new modified CSV file for training usage
-df.to_csv('normalized.csv')
+df.to_csv('data/normalized.csv')
 
 logger.info("Saved transformations to 'normalized.csv'")
 
