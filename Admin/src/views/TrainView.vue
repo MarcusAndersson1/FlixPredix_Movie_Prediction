a9bfddd5250882c4e0c7ff83f99372fa5709705a
adamfris <adamfris@student.chalmers.se>
2022-12-03 12:28:04 +0100

    Functionality for reading csv files

--- Admin/src/views/TrainView.vue
+++ Admin/src/views/TrainView.vue
@@ -1,27 +1,43 @@
 <script>
-import Header from "../components/Header.vue"
-import File from "../components/FileUpload.vue"
+import Header from "../components/Header.vue";
+import File from "../components/FileUpload.vue";
 
 export default {
+  mounted() {
+    const inputElement = document.getElementById("dropzone-file");
+
+    let data;
+    function handleFiles() {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        console.log(reader.result);
+      };
+
+      reader.readAsBinaryString(this.files[0].slice(0,50));
+
+    }
+    inputElement.addEventListener("change", handleFiles, false);
+
+  },
+
   components: {
     Header,
-    File
-  }
-}
+    File,
+  },
+};
 </script>
 
 
 <template>
-    <main>    
-        <header>
-            <Header />
-        </header>
-       
-        <File />
+  <main>
+    <header>
+      <Header />
+    </header>
 
-    </main>
+    <File />
+  </main>
 </template>
 
 <style scoped>
-
 </style>
