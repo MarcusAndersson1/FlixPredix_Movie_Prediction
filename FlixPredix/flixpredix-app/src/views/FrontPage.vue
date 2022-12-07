6ddf7a6ea521fd9bfb5fff4324c619e9efea5a6c
DaddyFresh <marcus.ge.andersson@gmail.com>
2022-12-07 19:54:32 +0100

    push so i can delete and clone a new project

--- FlixPredix/flixpredix-app/src/views/FrontPage.vue
+++ FlixPredix/flixpredix-app/src/views/FrontPage.vue
@@ -5,6 +5,14 @@
     <div class="divett">
       <h1 class="name">FLIXPREDIX</h1>
     </div>
+    
+    <v-btn elevation="9" icon x-large></v-btn>
+
+
+    <a href="/home">
+      <div class="triangle"></div>
+    </a>
+    <div class="HELP"></div>
     </body>
 </template>
 
@@ -15,6 +23,26 @@
   
   <style>
 .name {
-  font-size: x-large;
+     font-family: "aliens_and_cows";
+     font-size: 150px;
 }
+.triangle {
+  margin: 100px 650px;
+  height:0;
+  width:0;
+  border-top: 80px solid transparent;
+  border-right: 100px solid transparent;
+  border-bottom: 80px solid transparent;
+  border-left: 150px solid rgb(147, 19, 19);
+}
+.HELP {
+  margin: 500px 650px;
+  height:0;
+  width:0;
+  border-top: 80px solid transparent;
+  border-right: 100px solid transparent;
+  border-bottom: 80px solid transparent;
+  border-left: 150px solid transparent;
+}
+
   </style>
