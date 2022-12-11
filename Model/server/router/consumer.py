f44296cf0ddf607c7dd0019d483c1d3fade8a84a
Mislav Milicevic <mislav.milicevic22@gmail.com>
2022-12-11 14:11:01 +0100

    Add prediction body model

--- Model/server/router/consumer.py
+++ Model/server/router/consumer.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from enum import Enum
+from typing import Set
+
 
 __all__ = [
     'get_router'
@@ -9,9 +13,46 @@ router = APIRouter(
     prefix='/consumer'
 )
 
+class Genre(Enum):
+    action = 'action'
+    adventure = 'adventure'
+    animation = 'animation'
+    comedy = 'comedy'
+    crime = 'crime'
+    documentary = 'documentary'
+    drama = 'drama'
+    family = 'family'
+    fantasy = 'fantasy'
+    foreign = 'foreign'
+    history = 'history'
+    horror = 'horror'
+    music = 'music'
+    mystery = 'mystery'
+    romance = 'romance'
+    science_fiction = 'science_fiction'
+    thriller = 'thriller'
+    tv_movie = 'tv_movie'
+    war = 'war'
+    western = 'western'
+
+class Region(Enum):
+    africa = 'AF'
+    asia = 'AS'
+    europe = 'EU'
+    north_america = 'NA'
+    oceania = 'OC'
+    south_america = 'SA'
+    united_kingdom = 'UK'
+
+class PredictionData(BaseModel):
+    budget: int
+    runtime: int
+    genres: Set[Genre]
+    regions: Set[Region]
+
 
 @router.post('/predict')
-def predict():
+def predict(prediction_data: PredictionData):
     raise HTTPException(status_code=501, detail='Not implemented')
 
 
