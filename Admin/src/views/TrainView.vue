d064899cbb5c9b0088140689dba68e18354a4eb5
felixmertala <felixmer@student.chalmers.se>
2022-12-28 13:41:24 +0100

    base for uploading data

--- Admin/src/views/TrainView.vue
+++ Admin/src/views/TrainView.vue
@@ -1,24 +1,22 @@
 <script>
-
 import File from "../components/FileUpload.vue";
-import axios from "axios"
+import axios from "axios";
 
-function saveToDb(data) {
-  
-}
+function saveToDb(data) {}
 
 export default {
-  methods:{
-    async getDbRows(){
-  
-        const response = await axios.get('http://localhost:4000/get')
-        if (response.data){
-          console.log(response)
-        }else{
-          console.log('nothing')
-        };   
-
-    }
+  methods: {
+    async getDbRows() {
+      const response = await axios.get("http://localhost:4000/get");
+      if (response.data) {
+        console.log(response);
+      } else {
+        console.log("nothing");
+      }
+    },
+    async replaceModel(){},
+    async trainModel(){},
+    async validateModel(){},
   },
   mounted() {
     const inputElement = document.getElementById("dropzone-file");
@@ -28,7 +26,7 @@ export default {
       const reader = new FileReader();
 
       reader.onload = () => {
-        saveToDb(reader.result)
+        saveToDb(reader.result);
         console.log(reader.result.slice(0, 50));
       };
 
@@ -38,35 +36,78 @@ export default {
   },
 
   components: {
-     File,
+    File,
   },
 };
 </script>
 
-
 <template>
   <main>
+    <br />
+    <!-- calidate och train nedanför(ladda upp csv) -->
+    <div
+      class="p-6 max-w-lg mx-auto bg-purple-40 rounded-xl shadow-md flex items-center space-x-4"
+    >
+      <div>
+        <input
+          type="file"
+          class="file-input file-input-bordered w-full max-w-xs"
+        />
+      </div>
 
-
-    <br>
-    <File />
-    <br>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+      <button
+        @click="validateModel()"
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Validate Model
+      </button>
+      <button
+        @click="trainModel()"
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Train Model
+      </button>
+    </div>
+    <div
+      class="p-6 max-w-lg mx-auto bg-purple-40 rounded-xl shadow-lg flex items-center space-x-4"
+    >
+      <!-- byt ut modell(ladda upp modell) -->
+      <br/>
+      <div>
+        <input
+          type="file"
+          class="file-input file-input-bordered w-full max-w-xs"
+        />
+      </div>
+      <button
+        @click="replaceModel()"
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        Replace Model
+      </button>
+    </div>
+    <br />
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+    >
       Felix
     </button>
 
-    <button @click="getDbRows()" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    <button
+      @click="getDbRows()"
+      class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+    >
       Train Model
     </button>
-</main>
+  </main>
 </template>
 
 <style scoped>
-  main{
-    text-align: center;
-  }
+main {
+  text-align: center;
+}
 
-  button{
-    margin: 5px;
-  }
+button {
+  margin: 5px;
+}
 </style>
