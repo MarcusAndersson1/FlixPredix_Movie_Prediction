5ad54b984b501d7e0824eed0b6f8b231c2406b46
adamfris <adamfris@student.chalmers.se>
2023-01-05 17:28:54 +0100

    Dockerize UI page

--- FlixPredix/flixpredix-app/src/router/index.js
+++ FlixPredix/flixpredix-app/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import StartPage from '../views/StartPage.vue'
 import CredentialsPage from '../views/CredentialsPage.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
