96097ed42618262ea2edfc33bf4376141b306905
Ed <cooljoller@gmail.com>
2023-01-06 15:53:21 +0100

    Removed SignUp and SignIn

--- Admin/src/components/Header.vue
+++ Admin/src/components/Header.vue
@@ -9,16 +9,12 @@
             </a>
           </div>
   
-          <PopoverGroup as="nav" class="hidden space-x-10 md:flex">
+          <PopoverGroup as="nav" class="hidden items-center space-x-10 md:flex">
   
             <a href="/" class="text-base font-medium text-gray-500 hover:text-gray-900">Home</a>
             <a href="/#/analytics" class="text-base font-medium text-gray-500 hover:text-gray-900">Analytics</a>
 
           </PopoverGroup>
-          <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-            <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
-            <a href="#" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
-          </div>
         </div>
       </div>
   
